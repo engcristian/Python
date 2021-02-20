dad8d713c731e23e3541a65f5827141c379d331a
Cristian Santiago <eng.cristian.silva@gmail.com>
2021-02-20 18:40:55 -0300

    lists

--- python-codes/0_test.py
+++
@@ -1,7 +0,0 @@
-valores = []
-valores.append(1)
-valores.append(2)
-valores.append(3)
-for c, v in enumerate(valores):
-    print(f'na posição {c} está o número {v}')
-print('fim')
--- python-codes/78-List_1.py
+++ python-codes/78-List_1.py
@@ -2,3 +2,13 @@
 Reading 5 numbers and including them in a list, and at the end show the lower
 the higher and in wich position are they.
 '''
+my_list = list()
+count = 0
+
+for num in range(0,5):
+    my_list.append(input(f'Type the {num+1}º number: '))
+
+print(f'The list created is: {my_list}')
+print(f'The lower value is {min(my_list)}, and it is in the {my_list.index(min(my_list))+1}º position.')
+
+print(f'The higher value is {max(my_list)}, and it is in the {my_list.index(max(my_list))+1}º position.')
