d5093f52576cf95452967498fb138712a7781dcc
unknown <eng.cristian.silva@gmail.com>
2021-02-21 21:56:34 -0300

    Lists

--- python-codes/78-List_1.py
+++ python-codes/78-List_1.py
@@ -2,13 +2,16 @@
 Reading 5 numbers and including them in a list, and at the end show the lower
 the higher and in wich position are they.
 '''
-my_list = list()
+#creating an empty list;
+my_list = []
 count = 0
-
+#Iserting 5 numbers in a list, so doing  for in a range 0 to 5;
 for num in range(0,5):
-    my_list.append(input(f'Type the {num+1}º number: '))
+    my_list.append(int(input(f'Type the {num+1}º number: '))) #append to add at the end;
+#looking for the max and minimun value;
+max_num = max(my_list)
+min_num = min(my_list)
 
 print(f'The list created is: {my_list}')
-print(f'The lower value is {min(my_list)}, and it is in the {my_list.index(min(my_list))+1}º position.')
-
-print(f'The higher value is {max(my_list)}, and it is in the {my_list.index(max(my_list))+1}º position.')
+print(f'The lower value is {min_num}, and it is in the {my_list.index(min_num)+1}º position.')
+print(f'The higher value is {max_num}, and it is in the {my_list.index(max_num)+1}º position.')
--- python-codes/79-List_2.py
+++ python-codes/79-List_2.py
@@ -2,3 +2,16 @@
 Typing many numbers and including in a list, if this number already exist
 it won't be add, at the end, all add numbers will be shown (crescent order)
 '''
+#Creating an empty list;
+my_list = []
+#Using infinite looping to get how many numbers you want;
+while True:
+    my_list.append(int(input("Type numbers as you want and -0 to check the list: " )))
+    #Setting the stop condition;
+    if -0 in my_list:
+        #Removing the -0 condition;
+        my_list.pop()        
+        #Getting the right list excluding the duplicates and putting in crescent order;
+        final_list = sorted(set(my_list))
+        print(f"Here is the final list : {final_list}")
+        break
