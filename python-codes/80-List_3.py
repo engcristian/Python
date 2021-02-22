a059d0c82bca898ad6707ee2c01d82f9ff00fee2
unknown <eng.cristian.silva@gmail.com>
2021-02-22 01:48:08 -0300

    lists

--- python-codes/80-List_3.py
+++ python-codes/80-List_3.py
@@ -2,3 +2,12 @@
 Insert 5 numbers in the right position without using .sort()
 Then show the list orderned
 '''
+my_list = []
+
+while True:
+
+    my_list.append(int(input('Type any number: ')))
+    if -0 in my_list:
+        my_list.pop()
+        order_list = sorted(my_list)
+        print(f'Ordered list: {order_list}.')
--- python-codes/81-List_4.py
+++ python-codes/81-List_4.py
@@ -1,6 +1,28 @@
 '''
-Reqading many numbers and insert in a list
+Reading many numbers and insert in a list
 1 - how many numbers were typed?
-2- list in reverse- truw
-3- if the number 5 was typed and is inside or no the list
+2- list in reverse - true
+3- if the number 5 was typed and is inside or not the list
 '''
+
+my_list = []
+count = 0
+
+while True:
+    my_list.append(int(input('Type any number: ')))
+    
+    count+= 1
+    
+    if -0 in my_list:
+        
+        my_list.pop() 
+        my_list.sort(reverse = True)       
+        print(f'Were typed {count-1} numbers in this list.')
+        print(f'The reverse list is {my_list}')
+        
+        if 5 in my_list:            
+            print(f'Was found the number 5 in the {my_list.index(5)+1} position.')
+
+        elif 5 not in my_list:
+            print('Was not found the number 5')
+        break
