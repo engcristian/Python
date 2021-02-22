5c3ce6ce9445ab4a0a9f925a0b4aa0360a52265e
unknown <eng.cristian.silva@gmail.com>
2021-02-22 03:10:05 -0300

    Lists

--- python-codes/82-List_5.py
+++ python-codes/82-List_5.py
@@ -4,3 +4,17 @@ one list will have the EVEN numbers
 the other will have the ODD ones
 SHow the result
 '''
+odd_list = []
+even_list = []
+
+while True:
+    num = int(input('Type a number: '))
+    if num%2 == 0:
+        even_list.append(num)
+    elif num%2 != 0:
+        odd_list.append(num)
+    if num == -0:
+        even_list.pop()
+        print(f'The EVEN list: {even_list}')
+        print(f'The ODD list: {odd_list}')
+        break
