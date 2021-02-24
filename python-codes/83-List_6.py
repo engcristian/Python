54948d47fc7762f44c6b588c5e3645aa458e9d6d
unknown <eng.cristian.silva@gmail.com>
2021-02-24 02:46:21 -0300

    lists

--- python-codes/83-List_6.py
+++ python-codes/83-List_6.py
@@ -1,2 +1,19 @@
 '''
+Checking the expression's parentheses
 '''
+
+expression = str(input('Type a math expression with parentheses: '))
+par_list = [] # parentheses list
+for simbol in expression:
+    if simbol =='(':
+        par_list.append('(')
+    elif simbol == ')':
+        if len(par_list)>0:
+            par_list.pop()
+        else:
+            par_list.append(')')
+            break
+if par_list == 0:
+    print('Your expression is right.')
+else:
+    print("Your expression isn't right.")
