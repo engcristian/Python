10d2a4ccc22a838e4e633e8674d882fe24ea21c1
unknown <eng.cristian.silva@gmail.com>
2021-02-28 20:17:53 -0300

    Update 88-ex copy 5.py

--- python-codes/88-ex copy 5.py
+++ python-codes/88-ex copy 5.py
@@ -5,14 +5,19 @@ sortear seis numeros entre 1 e 60 para cada jogo
 cadastrar tudo em uma lista composta
 '''
 from random import randint
-#game = []
+geral = []
 game = []
+n = 6
 jogada = int(input('Jogadas: '))
 while True:
-
-    for j in range(jogada):
-        for i  in range (6):
+    for j in range(jogada):  
+        for a  in range (n):
             num = randint(1,60)
-            game.append(num)
-    print('QUEr continuar?')
-print(game)
+            geral.append(num)
+    
+        splited = [geral[i::n] for i in range(n)]
+        print(splited)
+        break
+
+    
+    
