19a0eebb91ec0e2dd71f8260f9347397d267a241
unknown <eng.cristian.silva@gmail.com>
2021-02-28 04:17:31 -0300

    nothing

--- python-codes/0_test.py
+++ python-codes/0_test.py
@@ -1,10 +1,2 @@
-import matplotlib.pyplot as plt
-X = [[1.,1.], [2.,2.], [3.,3.]]
-Y = [[1.,1.], [2.,2.], [3.,3.]]
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(X, Y)
-ax.set_xlabel(r'$\rho$')
-ax.yaxis.set_major_formatter('${x:.2f}')
-ax.xaxis.set_major_formatter('R${x:.2f}')
-plt.show()
+import itertools
+print([i for i in ifilter(lambda x: x%5, islice(count(5),10))])
