df7930560ca948e8f852cce3a70a7f9c74a52435
Cristian Santiago <eng.cristian.silva@gmail.com>
2021-03-06 17:40:07 -0300

    Testing lottery

--- Python-codes-CeV/88-Lottery_num.py
+++ Python-codes-CeV/88-Lottery_num.py
@@ -5,10 +5,12 @@ sortear seis numeros entre 1 e 60 para cada jogo
 cadastrar tudo em uma lista composta
 '''
 from random import randint
+my_game = [1,5,17,16,39,44]
 num_list = []
 game_list = []
 amount = int(input('How many games do you want? '))
 total = 1
+tot = 0
 while total <= amount:
     count = 0
     while True:
@@ -22,5 +24,10 @@ while total <= amount:
     game_list.append(num_list[:])
     num_list.clear() 
     total += 1   
-    for i, l in enumerate(game_list):
-        print(l)
+
+for i, l in enumerate(game_list):
+    print(f'{i+1}º game = {l}')
+    if l == my_game:
+        
+        tot +=1
+print(tot)
--- Python-codes-CeV/89-Student_grades.py
+++ Python-codes-CeV/89-Student_grades.py
@@ -5,18 +5,16 @@ Show a final grade from each student with the average grade
 first show the final grade
 Then ask if want to get the individual grade
 '''
+main_grade = list()
 while True:
     name = str(input('Name: '))
     grade1 = float(input('First grade: '))
     grade2 = float(input('Second grade: '))
     avg = (grade1 + grade2)/2
-    main_grade = list()
-
     main_grade.append([name,[grade1, grade2], avg])
-    opt = str(input("Continue? [Y/N] "))
-    if 'Nn' in opt:
+    opt = str(input("Continue? [Y/N] ")).lower()
+    if 'n' in opt:
         break
 for i, a in enumerate(main_grade):
-    print(f'{i}>>{a[0]}>>{a[2]}')
+    print(f'{i}: {a[0]:^20}:{a[2]}')
 
-print(main_grade)
