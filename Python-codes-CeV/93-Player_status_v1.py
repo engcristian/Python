dabc3045bc587e7dc4329bf9c9c5ba7614d7da75
unknown <eng.cristian.silva@gmail.com>
2021-03-16 23:01:27 -0300

    updating exercises

--- Python-codes-CeV/93-Player_status_v1.py
+++ Python-codes-CeV/93-Player_status_v1.py
@@ -7,22 +7,32 @@ inserting everything in one dict, including the total goals scored during the ch
 info = dict()
 goals_tot = []
 
-info['name'] = str(input('name: ')) 
-info['matches'] = int(input('matches: '))
+info["name"] = str(input("Player's name: ")) 
+info["matches"] = int(input('How many matches did he/she play? '))
 for i in range(info['matches']):          
-   goals_tot.append(int(input(f'Goals at the {i+1}º match: ')))
+   goals_tot.append(int(input(f'How many goals at the {i+1}º match: ')))
 
 info['goals per match'] = goals_tot
-
 info['total goals']= sum(goals_tot)
+opt =''
 print('-='*20)
-print(f"The field name has the value {info['name']}.")
-print(f"The field goals per match has the value {info['goals per match']}.")
-print(f"The field total goals has the value {info['total goals']}.")
+print(f"The field 'name' has the value {info['name']}.")
+print(f"The field 'goals per match' has the value {info['goals per match']}.")
+print(f"The field 'total goals' has the value {info['total goals']}.")
 print('-='*20)
-opt = str(input('Wish get details from that player? [Y/N]: ')).lower()
-if 'y' in opt:
-    print(f'The player {info["name"]} played {info["matches"]} matches.')
-    for i in range(info['matches']):
-        print(f'On the {i+1}º match he scored {info["goals per match"][i]} goal(s).')
-    print(f"In total he scored: {info['total goals']} goals")    
+while 'y' or 'n' in opt:
+    opt = str(input('Do you wish to get details from that player? [Y/N]: ')).lower()
+    
+    if 'y' in opt:
+        print(f'The player {info["name"]} played {info["matches"]} matches.')
+        for i in range(info['matches']):
+            print(f'On the {i+1}º match he/she scored {info["goals per match"][i]} goal(s).')
+        print(f"In total he/she scored: {info['total goals']} goals")
+        break
+        
+    if 'n' in opt:
+        print('Thank you! Come back often.')
+        break
+    else:
+        print('Sorry, the options are Y/N.')
+
--- Python-codes-CeV/94-People_dict.py
+++ Python-codes-CeV/94-People_dict.py
@@ -19,3 +19,44 @@ Ex.
 >> nome = ana; sexo=F; idade = 25;
 
 '''
+reg = {}
+cad = []
+idade = []
+media = 0
+mulheres = []
+acima_media = []
+cont_p = 0
+
+
+while True:
+
+    
+    reg['nome'] = str(input('nome: '))
+
+    reg['sexo'] = str(input('sexo M/F: ')).lower()
+
+    reg['idade'] = int(input('idade: '))
+    if reg['idade'] > media:
+        acima_media.append(reg['nome'])
+        acima_media.append(reg['sexo'])
+        acima_media.append(reg['idade'])
+    cont_p +=1
+    cad.append(reg.copy())
+    idade.append(reg['idade'])
+    media = sum(idade)//len(idade)
+    if reg['sexo'] == 'f':
+        mulheres.append(reg['nome'])
+    opt = str(input('Deseja continuar? Y/N  ')).lower()
+    if 'n' in opt:
+        break
+
+
+
+
+print(cont_p)
+print(media)
+print(mulheres)
+print(acima_media)
+
+
+
